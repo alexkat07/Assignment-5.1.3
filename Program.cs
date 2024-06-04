c593ae56a5afe88a5dc2efb05e5fc7ec753fe240
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-04 11:13:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+namespace Assignment_5._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsDuplicate([1, 2, 3, 1]));
+
+            bool IsDuplicate(int[] input) 
+            {
+                Dictionary <int, int> duplicates = new Dictionary<int, int>();
+
+                foreach (int i in input) 
+                {
+                    if (!duplicates.ContainsKey(i))
+                    {
+                        duplicates.Add(i, i);
+                    }
+                    else 
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
